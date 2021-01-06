48bde6fbf4a8169f724350402d9853607535143d
prashantsachan98 <prashantsachan03@gmail.com>
2021-01-06 13:04:55 +0530

    fetch data from API

--- lib/service/api_services.dart
+++ lib/service/api_services.dart
@@ -1,18 +1,1 @@
-/*import 'dart:convert';
-import 'dart:io';
-import 'package:foodose/models/recipe.dart';
-import 'package:http/http.dart' as http;
-
-class ApiService {
-  ApiService._instantiate();
-  static final ApiService instance = ApiService._instantiate();
-
-
-
-
-
-
-
-
-  
-}*/
+//will use it for services later
